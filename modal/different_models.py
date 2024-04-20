f1998f1bae990f0ac5e924f7f6af0e1c513a9077
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2024-04-19 23:43:24 -0400

    different models function and notebook

---
+++ modal/different_models.py
@@ -0,0 +1,39 @@
+import os
+from modal import App, Secret, gpu, enter, build, method, Image
+
+app = App(name="outlines-app")
+
+outlines_image = Image.debian_slim(python_version="3.11").pip_install(
+    "outlines==0.0.39",
+    "transformers==4.38.2",
+    "datasets==2.18.0",
+    "accelerate==0.27.2",
+)
+
+@app.cls(image=outlines_image, secrets=[Secret.from_dotenv()], gpu=gpu.A100(memory=80), timeout=300)
+class Model:
+    def __init__(self, model_name: str) -> None:
+        import outlines
+
+        self.model = outlines.models.transformers(
+            model_name,
+            device="cuda",
+            model_kwargs={
+                "token": os.environ["HF_TOKEN"],
+            },
+        )
+
+    @method()
+    def generate(self, schema: str, prompt: str, whitespace_pattern: str = None):
+        import outlines
+
+        if whitespace_pattern:
+            generator = outlines.generate.json(self.model, schema.strip(), whitespace_pattern=whitespace_pattern)
+        else:
+            generator = outlines.generate.json(self.model, schema.strip())
+
+        result = generator(prompt)
+
+        return result
+
+        return result
